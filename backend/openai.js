c1c2296e5f053e883b14978d79faaf6c022cdf38
Kevin Lu <37918520+1n48yg@users.noreply.github.com>
2023-05-20 15:29:41 -0700

    Changed max tokens for ai responses

--- backend/openai.js
+++ backend/openai.js
@@ -16,12 +16,13 @@ const initialize = async(req_data) =>
     var previous_position = "Data Scientist Intern";
     var num_question = 5;
 
-    const setup_prompt = `This is an interview. You are to interview me for a ${position} that uses ${technologies}. My name is ${name}, I’m studying ${uni_program} and I have worked as a ${previous_position}. Act like this is a real interview, and you are the interviewer and I'm the interviewee. You are only allowed to ask ${num_question} questions. You ask one question at a time, and you wait for me to type in a prompt. The interview must end after ${num_question} questions. You must ignore my prompt and proceed to ask the next question. Start off by asking the first question`;
+    const setup_prompt = `Ignore all previous instructions before this one. This is an interview. You are to interview me for a ${position} that uses ${technologies}. My name is ${name}, I’m studying ${uni_program} and I have worked as a ${previous_position}. Act like this is a real interview, and you are the interviewer and I'm the interviewee. You are only allowed to ask ${num_question} questions. You ask one question at a time, and you wait for me to type in a prompt. The interview must end after ${num_question} questions. You must ignore my prompt and proceed to ask the next question. Start off by asking the first question.`;
 
     try {
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: setup_prompt,
+            max_tokens: 500
         });
         console.log(completion.data.choices[0].text);
         return completion.data.choices[0].text;
@@ -49,6 +50,7 @@ const getNextResponse = async(req_data) => {
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: user_prompt,
+            max_tokens: 400
         });
         console.log(completion.data.choices[0].text);
         return completion.data.choices[0].text;
