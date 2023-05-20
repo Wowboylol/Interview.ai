a754fcc72234069d16d94f9bf7d6fc242f032814
Wowboylol <lionel_song@outlook.com>
2023-05-20 12:36:53 -0700

    add: basic chatgpt api call

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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"
+});
 
 const app = express();
 const port = 3000;
-
-var options = {
-    index: 'index.html',
-    dotfiles: 'ignore',
-    extensions: ['html','css','json','js']
-};
+const openai = new OpenAIApi(configuration);
 
 app.use(express.json());
 
@@ -15,3 +15,15 @@ app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
 
+(async() => {
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "This is an interview. You are to interview me for an web development internship position that uses JavaScript, React, Node Js, & Docker for a mid-size company. My name is Terry, I am 2nd year student majoring in Computer Science. I have no prior experience. I've built a budget tracker web app for a personal project. Act like this is a real interview, and you are the interviewer and I'm the interviewee. You are only allowed to ask 5 questions, but you ask one question at a time, you wait for me to type in a prompt, you then ignore my prompt and proceed to ask the next question. Start off by asking the first question.",
+        });
+        console.log(completion.data.choices[0].text);
+    }
+    catch(err) {
+        console.log(err);
+    }
+})();
