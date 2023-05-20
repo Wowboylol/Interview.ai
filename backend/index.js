276df8403857c4c189700e668aa281a7020f3ff5
Wowboylol <lionel_song@outlook.com>
2023-05-20 13:15:16 -0700

    add: openai file

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: "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"
-});
+const ai = require('./openai');
 
 const app = express();
 const port = 3000;
-const openai = new OpenAIApi(configuration);
 
 app.use(express.json());
+
+app.get('/api/prompt', (req, res) => {
+    console.log(req.query);
+});
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
 
 (async() => {
-    try {
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: "This is an interview. You are to interview me for an web development internship position that uses JavaScript, React, Node Js, & Docker for a mid-size company. My name is Terry, I am 2nd year student majoring in Computer Science. I have no prior experience. I've built a budget tracker web app for a personal project. Act like this is a real interview, and you are the interviewer and I'm the interviewee. You are only allowed to ask 5 questions, but you ask one question at a time, you wait for me to type in a prompt, you then ignore my prompt and proceed to ask the next question. Start off by asking the first question.",
-        });
-        console.log(completion.data.choices[0].text);
-    }
-    catch(err) {
-        console.log(err);
-    }
+    const response = await ai.test();
 })();
---
+++ backend/openai.js
@@ -0,0 +1,24 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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"
+});
+
+const openai = new OpenAIApi(configuration);
+
+const test = async() => {
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "This is an interview. You are to interview me for an web development internship position that uses JavaScript, React, Node Js, & Docker for a mid-size company. My name is Terry, I am 2nd year student majoring in Computer Science. I have no prior experience. I've built a budget tracker web app for a personal project. Act like this is a real interview, and you are the interviewer and I'm the interviewee. You are only allowed to ask 5 questions, but you ask one question at a time, you wait for me to type in a prompt, you then ignore my prompt and proceed to ask the next question. Start off by asking the first question.",
+        });
+        console.log(completion.data.choices[0].text);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    test
+};
