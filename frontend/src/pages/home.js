f8676ec0a15b5114f5e417913109b64ea157dc49
tfu2003 <officialtonyfu@gmail.com>
2023-05-20 18:27:30 -0700

    minor change

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 font-serif">
       <h1 className="text-4xl text-center justify-center">Interview.ai</h1>
-      <p> An application to practice for your important interviews</p>
+      <p> An application that allows you to practice for your important interviews</p>
       <div className="pt-4 space-x-12">
         <button
           className="border-2 border-black rounded-full shadow hover:shadow-md hover:opacity-50 p-4 transition duration-500"
