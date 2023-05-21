488e9f14cb2384bde4046fd6f7c25ba52cf8b21a
Kevin Lu <37918520+1n48yg@users.noreply.github.com>
2023-05-20 20:34:45 -0700

    Cleaned up memory code, and aligned setup for frontend

--- backend/index.js
+++ backend/index.js
@@ -12,17 +12,13 @@ app.post('/api/start', async (req, res) => {
     console.log(req.body);
     ai.clearMemory();
     const response = await ai.initialize(req.body.position, req.body.name, req.body.technologies);
-    ai.appendToMemory(" Here is the chat log so far: You: " + response + " ");
     res.status(200).json({
         message: response
     })
 });
 
 app.get('/api/prompt', async (req, res) => {
-    var user_input = ai.getMemory() + " What is a data science?"; //temporary variable for testing
-    ai.appendToMemory("Me: " + user_input + " ");
-    const response = await ai.getNextResponse(user_input);
-    ai.appendToMemory("You: " + response + " ");
+    const response = await ai.getNextQuestion();
     res.status(200).json({
         message: response
      })
