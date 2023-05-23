3f7659cf2e00cc9a9a1cc5e411250434b450a56f
tfu2003 <officialtonyfu@gmail.com>
2023-05-23 15:39:58 -0700

    added loader component

--- frontend/src/pages/practice.js
+++ frontend/src/pages/practice.js
@@ -5,6 +5,7 @@ import SpeechRecognition, {
 import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
 import { useLocation, useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
+import Loader from "../components/loader";
 
 // const appId = process.env.REACT_APP_SECRET;
 const appId = "7fd27f79-99ff-4484-879a-5b776ba182d1";
@@ -23,7 +24,6 @@ function Practice() {
   const data = location.state["data"]["message"];
   const {
     transcript,
-    listening,
     browserSupportsSpeechRecognition,
     isMicrophoneAvailable,
     resetTranscript,
@@ -88,16 +88,10 @@ function Practice() {
         <div className="text-center flex flex-col items-center gap-10 font-serif text-bold font-semibold">
           <h1 className="pt-10 text-3xl">{data[index]}</h1>
           {loader ? (
-            <div
-              class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-              role="status"
-            >
-              <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                Loading...
-              </span>
-            </div>
+            <Loader/>
           ) : (
-            <div> </div>
+            <div>
+            </div>
           )}
           <Webcam height="200" width="400" onUserMedia={handleLoader} />
           <p className="text-x w-2/3">{transcript}</p>
